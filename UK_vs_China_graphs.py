387d321d8e9c34ded98d081cbacb0ed69e168997
phizzii <sophiebotten06@gmail.com>
2025-10-12 09:02:52 +0100

    dropping unnecessary columns and creating masks to filter for specific years

--- UK_vs_China_graphs.py
+++ UK_vs_China_graphs.py
@@ -9,4 +9,15 @@ import seaborn as sns
 uk_china_deaths_df = pd.read_csv("UK_vs_China_Death_Data.csv")
 uk_china_annual_working_hours_df = pd.read_csv("annual-working-hours-per-worker.csv")
 
+# applying head function to see current columns to decide which to drop
 print(uk_china_deaths_df.head())
+print(uk_china_annual_working_hours_df.head())
+
+# dropping unnecessary columns from death dataset
+uk_china_deaths_df.drop(columns=['measure_id','location_id','sex_id','age_id','cause_id','metric_id','upper','lower'])
+
+# creating mask for specific years
+mask1 = uk_china_deaths_df['year'] >= 2011
+uk_china_deaths_2011_to_2019_df = uk_china_deaths_df[mask1]
+
+print(uk_china_deaths_2011_to_2019_df)
