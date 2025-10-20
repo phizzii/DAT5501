a781b416e4a698e814be5fa44f5f9d1fe9e6996a
phizzii <sophiebotten06@gmail.com>
2025-10-20 10:44:54 +0100

    writing the task notes down to plan the programming

---
+++ duration_calculator_python.py
@@ -0,0 +1,5 @@
+# write code to take a date input from the user and calculate how many days until today
+# unit testing: build a test suite to validate your calculation
+# code extension: load the dates from the random_dates.csv file on qm+
+# write a function to calculate how many days in the past each date it
+# hint use np.datetime64
