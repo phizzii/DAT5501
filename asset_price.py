3bf911657f04c6712de40224b69ffd8c9750e66c
phizzii <sophiebotten06@gmail.com>
2025-10-20 12:26:35 +0100

    new file for asset price task

---
+++ asset_price.py
@@ -0,0 +1,6 @@
+# choose a company on Nasdaq
+# download 1 year of historical price data
+# clean dataset as necessary
+# plot closing price vs date
+# extra: calculate daily percentage change, plot vs date
+# extra extra: calculate standard deviation of percent daily changes
