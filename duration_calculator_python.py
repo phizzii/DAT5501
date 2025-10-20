da9da1989e2e2c957927ac4c95a07e96c54a558e
phizzii <sophiebotten06@gmail.com>
2025-10-20 10:56:37 +0100

    created duration calculator

--- duration_calculator_python.py
+++ duration_calculator_python.py
@@ -3,3 +3,16 @@
 # code extension: load the dates from the random_dates.csv file on qm+
 # write a function to calculate how many days in the past each date it
 # hint use np.datetime64
+
+import numpy as np
+
+desired_date = input('input date (not current) in the format yyyy-mm-dd. e.g. 2007-03-09')
+desired_date_in_dt = np.datetime64(desired_date)
+print(desired_date_in_dt)
+
+current_date = np.datetime64('today')
+print(current_date)
+
+days_difference = current_date - desired_date_in_dt
+
+print(days_difference)
