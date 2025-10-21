acd35d8c2e3165e3b3fb612b4945fd6e041848c5
phizzii <sophiebotten06@gmail.com>
2025-10-21 19:16:13 +0100

    update comments for us election task and importing relevant modules in asset price task

--- asset_price.py
+++ asset_price.py
@@ -4,3 +4,14 @@
 # plot closing price vs date
 # extra: calculate daily percentage change, plot vs date
 # extra extra: calculate standard deviation of percent daily changes
+
+from matplotlib import markers
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy
+import seaborn as sns
+
+# creating relevant dataframe for historical data on Rocket Lab asset price
+
+rocket_lab_historical_data = pd.read_csv("")
---
+++ us_elections_activity.py
@@ -0,0 +1,3 @@
+# download us election data on qm+
+# load file into a pandas dataframe
+# plot a histogram of the fraction of votes
