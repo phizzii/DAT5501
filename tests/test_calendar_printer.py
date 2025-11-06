bee485bf2f0a5ae4abdf74f31edaa5da49b982e3
phizzii <sophiebotten06@gmail.com>
2025-11-06 11:23:27 +0000

    fixing file path issues for circle ci

--- tests/test_calendar_printer.py
+++ tests/test_calendar_printer.py
@@ -1,4 +1,7 @@
 # calendar printer testing suite
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import calender_printer_python
 
 import builtins
 from io import StringIO
