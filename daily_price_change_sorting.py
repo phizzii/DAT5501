e347313f2f3c66d466e8017ca77a402d4a41b5a9
phizzii <sophiebotten06@gmail.com>
2025-11-10 11:27:03 +0000

    adding relevant modules for data analysis and importing csv for historical data on rocket lab asset price data

--- daily_price_change_sorting.py
+++ daily_price_change_sorting.py
@@ -2,3 +2,17 @@
 # ^p = p(n+1) - p(n)
 # time how long it takes T to sort ^p for n = 7 > 365
 # plot T vs n, does it follow n log n distribution
+
+from matplotlib import markers
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy
+import seaborn as sns
+from sqlalchemy import true
+
+# creating relevant dataframe for historical data on Rocket Lab asset price
+
+rocket_lab_historical_data_df = pd.read_csv("HistoricalData_1761069203394.csv")
+
+# google how to do sorts in python (different sorts)
