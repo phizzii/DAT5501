a3a4ec08b4f2bc94ba364bcdfea2a54f7102c41f
phizzii <sophiebotten06@gmail.com>
2025-11-11 19:36:28 +0000

    added merge sort and insertion sort functions into script, testing before adding comments to make sure the script works as intended

--- daily_price_change_sorting.py
+++ daily_price_change_sorting.py
@@ -62,3 +62,47 @@ def bubble_sort(array):
             break
     
     return array
+
+def insertion_sort(array):
+    # looping from second element of the array until the last one
+    for i in range(1, len(array)):
+        key_item = array[i]
+
+        j = i - 1
+
+        while j >= 0 and array[j] > key_item:
+            array[j + 1] = array[j]
+            j -= 1
+
+            array[j + 1] = key_item
+        
+        return array
+    
+def merge_sort(left, right):
+    if len(left) == 0:
+        return right
+
+    if len(right) == 0:
+        return left
+    
+    result = []
+    index_left = index_right = 0
+
+    while len(result) < len(left) + len(right):
+        if left[index_left] <= right[index_right:
+            result.append(left[index_right])]:
+            index_left += 1
+        else:
+            result.append(right[index_right])
+            index_right += 1
+
+        if index_right == len(right):
+            result += left[index_left:]
+            break
+
+        if index_left == len(left):
+            result += right[index_right:]
+            break
+
+    return result
+            
