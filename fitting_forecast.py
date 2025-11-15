e28700da675ac1d6e902695f3ee15a1210b7d2cc
phizzii <sophiebotten06@gmail.com>
2025-11-15 09:33:12 +0000

    adding comments to script in preparation for creating graphs

---
+++ fitting_forecast.py
@@ -0,0 +1,5 @@
+# find dataset that measures a global trend over the past 10 years like population, temperature, gdp, life expectancy
+# sub sample all but the past 10 years of data
+# fit the sub-sample with polynomials from order 1 (a line) to 9 ax^n
+# forecast each polynomial 10 years into the future
+# how do they compare with reality
