a3e761de4f787953fcf7c9d085a6e1cba9b3e2f4
phizzii <sophiebotten06@gmail.com>
2025-11-16 15:01:01 +0000

    adding edge colours because i couldn't see the graph clearly

--- us_elections_activity.py
+++ us_elections_activity.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 us_election_df = pd.read_csv('us_election_dataset.csv', sep=';') # cause its separated by semi colons
 
-plt.hist(us_election_df['fraction_votes'])
+plt.hist(us_election_df['fraction_votes'], edgecolor='black')
 plt.xlabel('Fraction of votes')
 plt.ylabel('Count')
 plt.title('Histogram of vote fractions')
