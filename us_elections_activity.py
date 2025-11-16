93d24a25643a091327fce6472c8560c3369f1190
phizzii <sophiebotten06@gmail.com>
2025-11-16 15:08:51 +0000

    creating box plot and adding colours that do not look good but what can i do

--- us_elections_activity.py
+++ us_elections_activity.py
@@ -3,6 +3,7 @@
 # plot a histogram of the fraction of votes
 # compare vote fraction for two candidates
 
+from os import uname
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -12,5 +13,18 @@ plt.hist(us_election_df['fraction_votes'], edgecolor='black')
 plt.xlabel('Fraction of votes')
 plt.ylabel('Count')
 plt.title('Histogram of vote fractions')
+plt.show()
 
+candidate1 = us_election_df[us_election_df['candidate'] == 'Bernie Sanders']['fraction_votes']
+candidate2 = us_election_df[us_election_df['candidate'] == 'Ted Cruz']['fraction_votes']
+colors = ['green', 'orange']
+
+# i want to make a box plot lol
+
+myplot = plt.boxplot([candidate1, candidate2], labels=['Bernie Sanders', 'Ted Cruz'], patch_artist=True)
+for patch, color in zip(myplot['boxes'], colors):
+    patch.set_facecolor(color)
+
+plt.ylabel('Fraction of votes')
+plt.title('Vote fraction comparison Sanders vs Cruz')
 plt.show()
