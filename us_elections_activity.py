547cb25b690912b6325559388e0c0d10943e3d2c
phizzii <sophiebotten06@gmail.com>
2025-11-16 15:00:04 +0000

    add relevant modules and create histogram

--- us_elections_activity.py
+++ us_elections_activity.py
@@ -2,3 +2,15 @@
 # load file into a pandas dataframe
 # plot a histogram of the fraction of votes
 # compare vote fraction for two candidates
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+us_election_df = pd.read_csv('us_election_dataset.csv', sep=';') # cause its separated by semi colons
+
+plt.hist(us_election_df['fraction_votes'])
+plt.xlabel('Fraction of votes')
+plt.ylabel('Count')
+plt.title('Histogram of vote fractions')
+
+plt.show()
