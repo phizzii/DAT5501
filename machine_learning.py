13bdad8225b981d16c90503837c1039e8ed3f5f0
Sophie <109428682+phizzii@users.noreply.github.com>
2025-11-24 17:51:50 +0000

    creating new files for machine learning activity w10 and testing suite in preparation for creating a set of tests for future script.

--- machine_learning.py
+++ machine_learning.py
@@ -3,3 +3,6 @@
 # test-train split: ~70/30 test/train split is common
 # model selection & training: Choose models and train on training data
 # model evaluation & optimisation: Test on test data, evaluate and optimise performanc
+
+# do the guitar chord dataset from the uc irvine machine learning repo
+import sklearn
