02e2c7e8730379ffa61ec9fb0ed39456532cf48b
Sophie <109428682+phizzii@users.noreply.github.com>
2025-11-27 11:19:13 +0000

    updating machine learning files, adding testing file and adding csv file for guitar chord csv which i will be training ml model on

--- machine_learning.py
+++ machine_learning.py
@@ -4,5 +4,12 @@
 # model selection & training: Choose models and train on training data
 # model evaluation & optimisation: Test on test data, evaluate and optimise performanc
 
+# use a decision tree to classify and predict data
+# evaluate the model with precision and recall
+# identify a labelled dataset with a discrete outcome or classification
+# example outcomes: insurance claim, loan default, customer purchase
+# ideal dataset size: not too big not too small
+# eg, 2000 lines of data, 5 features
+
 # do the guitar chord dataset from the uc irvine machine learning repo
 import sklearn
---
+++ tests/test_machine_learning.py
@@ -0,0 +1,6 @@
+# use a decision tree to classify and predict data
+# evaluate the model with precision and recall
+# identify a labelled dataset with a discrete outcome or classification
+# example outcomes: insurance claim, loan default, customer purchase
+# ideal dataset size: not too big not too small
+# eg, 2000 lines of data, 5 features
