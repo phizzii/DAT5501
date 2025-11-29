a777e9ff1950a06be8cc2a78c5464b175654d873
Sophie <109428682+phizzii@users.noreply.github.com>
2025-11-29 10:05:29 +0000

    forgot to change the hard coded number of days lol

--- tests/test_duration_cal.py
+++ tests/test_duration_cal.py
@@ -19,7 +19,7 @@ def test_days_difference_correct(monkeypatch, capsys):
 
     assert "2000-01-01" in capture
     assert "today" not in capture
-    assert "9463" in capture # THIS NEEDS TO BE CONSISTENTLY CHANGED ALIGNING WITH THE CURRENT DATE
+    assert "9464" in capture # THIS NEEDS TO BE CONSISTENTLY CHANGED ALIGNING WITH THE CURRENT DATE
 
 def test_invalid_date_input(monkeypatch):
     inputs = iter(["not-a-date"])
